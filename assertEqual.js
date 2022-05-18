cd9039ecd9498efc21cff5a654a2e8daf9d8e6ba
duducesconetto <cesconettoedu@gmail.com>
2022-05-18 20:21:09 +0000

    assertEqual implement

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 

c3aa62f71dd944db1dd069d3fcaaa03a90534482
duducesconetto <cesconettoedu@gmail.com>
2022-05-18 20:22:24 +0000

    get first item

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+function head(firstItem) {
+  return firstItem[0];
+}
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

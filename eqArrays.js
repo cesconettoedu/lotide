e60eeda4bc940131b024e2045af0f04be280cb83
duducesconetto <cesconettoedu@gmail.com>
2022-05-20 19:04:26 +0000

    compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(a1, a2) {
+  if (a1.length !== a2.length) {
+    return false
+  }
+
+  for (let i = 0; i < a1.length; i++){
+    if (a1[i] !== a2[i]){
+      return false
+    }
+  }
+  return true
+}
+
+

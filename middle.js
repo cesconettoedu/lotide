54d1d7c0d6ea86f818139e1239f349699317f310
duducesconetto <cesconettoedu@gmail.com>
2022-05-21 23:03:43 +0000

    take in an array and return the middle-most element(s) of the given array

---
+++ middle.js
@@ -0,0 +1,40 @@
+const eqArrays = function(a1, a2) {
+  if (a1.length !== a2.length) {
+    return false
+  }
+ 
+  for (let i = 0; i < a1.length; i++){
+    if (a1[i] !== a2[i]){
+      return false
+    }
+  }
+  return true
+}
+ 
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const middle = function(array) {
+  let output = []
+   if(array.length < 3){
+    output = output;
+  } else if (array.length % 2 === 0) {
+    output.push(array[(array.length / 2)-1]);
+    output.push(array[(array.length / 2)]);
+  }else{
+    output.push(array[Math.floor(array.length/2)]) 
+  }
+  return output
+}
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]))
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]))
+// console.log(assertArraysEqual(middle([1, 2, 3]), [2])) // => [2]
+// console.log(assertArraysEqual(middle([1]), [])) // => []
+// console.log(assertArraysEqual(middle([1, 2]), [])) // => []
+// console.log(assertArraysEqual(middle([1, 2, 3]), [2])) // => [2]
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [1, 2])) // => [3, 4]

aea77fb8faf32291af9877254b90dec82299076c
duducesconetto <cesconettoedu@gmail.com>
2022-05-23 17:34:43 +0000

    Return a given array, removing unwanted elements.

--- without.js
+++ without.js
@@ -1,15 +1,15 @@
 const eqArrays = function(a1, a2) {
   if (a1.length !== a2.length) {
-    return false
+    return false;
   }
  
-  for (let i = 0; i < a1.length; i++){
-    if (a1[i] !== a2[i]){
-      return false
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] !== a2[i]) {
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
  
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -19,20 +19,20 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
  
-function without (source, itemsToRemove) {
+const without = function(source, itemsToRemove) {
   let arrayWithout = [];
-  for (let i =0; i <source.length; i++) {
-    if (!itemsToRemove.includes(source[i])){   //the instructor help me with includes
-      arrayWithout.push(source[i])
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {   //the instructor help me with includes
+      arrayWithout.push(source[i]);
     }
   }
-  return arrayWithout
-}
+  return arrayWithout;
+};
 
 
-// console.log(without([1, 2, 3], [1]))
-// console.log(assertArraysEqual(without([1, 2, 3], [1]),[2, 3]))
+console.log(without([1, 2, 3], [1]));
+assertArraysEqual(without([1, 2, 3], [1]),[2, 3]);
 
-// console.log(without(["1", "2", "3"], [1, 2, "3"]))
-// console.log(assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["3"]))
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["3"]);
 

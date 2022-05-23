98bcdc38a56725b4981905964a1673542488f192
duducesconetto <cesconettoedu@gmail.com>
2022-05-23 16:38:55 +0000

    compare two arrays for a perfect match.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,15 @@
 const eqArrays = function(a1, a2) {
   if (a1.length !== a2.length) {
-    return false
+    return false;
   }
 
-  for (let i = 0; i < a1.length; i++){
-    if (a1[i] !== a2[i]){
-      return false
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] !== a2[i]) {
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {

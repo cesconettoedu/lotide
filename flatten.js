f67d2afaf41a3a91f06155b9d5461d60bca6aa6b
duducesconetto <cesconettoedu@gmail.com>
2022-05-23 17:35:30 +0000

    return a flattened version of the array.

--- flatten.js
+++ flatten.js
@@ -1,15 +1,15 @@
 const eqArrays = function(a1, a2) {
   if (a1.length !== a2.length) {
-    return false
+    return false;
   }
  
-  for (let i = 0; i < a1.length; i++){
-    if (a1[i] !== a2[i]){
-      return false
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] !== a2[i]) {
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
  
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -19,18 +19,27 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
  
-function flatten (array1) {
-  if(Array.isArray(array1) === false) {
-    return "its not an array"
+const flatten = function(array1) {
+  if (Array.isArray(array1) === false) {
+    return "its not an array";
   }
-  let newArray = array1.flat(Infinity);
-  return newArray
-}
+  let newArray = [];
+  for (let i = 0; i < array1.length; i++) {
+    let item = array1[i];
+    if (typeof  item === "object") {
+      for (let j = 0; j < item.length; j++) {
+        newArray.push(item[j]);
+      }
+    } else {
+      newArray.push(item);
+    }
+  }
+  return newArray;
+};
  
  
-//console.log(flatten("eduardo"))
-//console.log(flatten([1, 2, [3, 4], 5, [6]]))
-//console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]))
- 
-// console.log(without(["1", "2", "3"], [1, 2, "3"]))
-// console.log(assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["3"]))
+// console.log(flatten("eduardo"));
+// console.log(flatten([1, 2, [3, 4], 5, [6]]));
+// console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]));
+// console.log(flatten(["1", "2", "3"], [1, 2, "3"]));
+// console.log(assertArraysEqual(flatten(["1", "2", "3"], [1, 2, "3"]), ["3"]));

69bd8eb85ae5761c9672ac00896326f71fd301d8
duducesconetto <cesconettoedu@gmail.com>
2022-05-27 17:40:03 +0000

    our own version of map

---
+++ map.js
@@ -0,0 +1,35 @@
+const eqArrays = function(a1, a2) {
+  if (a1.length !== a2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] !== a2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+
+assertArraysEqual((results1), ['g', 'c', 't', 'm', 't']);

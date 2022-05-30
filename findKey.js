961440292981eba6c6c08d603774ba3901f67060
duducesconetto <cesconettoedu@gmail.com>
2022-05-30 00:36:55 +0000

    for em obj find key

---
+++ findKey.js
@@ -0,0 +1,27 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = (namesX, callback) => {
+  for (const key in namesX) {
+    if (callback(namesX[key])) {
+      return key;
+    }
+  }
+};
+
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2),"noma")); // => "noma"
+

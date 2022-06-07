88a78b0d64c9565b7274f2b1f257dc411486a0c2
Eduardo Cesconetto <99515575+cesconettoedu@users.noreply.github.com>
2022-06-07 18:13:55 -0400

    return count of each of the letters in that sentence

--- countLetters.js
+++ countLetters.js
@@ -25,4 +25,4 @@ console.log(countLetters("lighthouse in the house"));
 //console.log(assertEqual(countLetters("lighthouse in the house"), {l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1}));
  
 //{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1,}
-
+ 

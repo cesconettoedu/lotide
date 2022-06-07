819bb71ebae7ce5a27ab944f3886beaa0d2de490
duducesconetto <cesconettoedu@gmail.com>
2022-06-07 23:42:58 +0000

    remove item, not modify array original - update clean

--- tail.js
+++ tail.js
@@ -1,15 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(rest) {
   return rest.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
+
+
+module.exports = tail;

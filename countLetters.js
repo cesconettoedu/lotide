83493a6fd60729b26a3fa4af17c934ea1b2da6de
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 18:26:41 +0000

    return count of each of the letters in that sentence

--- countLetters.js
+++ countLetters.js
@@ -1,17 +1,9 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
- 
+
 const countLetters = (array1) => {
   let output = {};
-  let arrayDiv = array1.replace(/\s/g, ''); // removi espaco
+  let arrayDiv = array1.replace(/\s/g, ''); 
   for (let value of arrayDiv) {
-    //console.log(typeof value);
-    if (value in output) {
+      if (value in output) {
       output[value] += 1;
     } else {
       output[value] = 1;
@@ -20,9 +12,5 @@ const countLetters = (array1) => {
   return output;
 };
  
-console.log(countLetters("lighthouse in the house"));
- 
-//console.log(assertEqual(countLetters("lighthouse in the house"), {l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1}));
- 
-//{ l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1,}
+
  

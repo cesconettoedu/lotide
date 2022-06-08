dccf7bc7a2fa2d44ab82c8a100697687e3e9fc56
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 17:10:13 +0000

    for em obj find key

--- findKey.js
+++ findKey.js
@@ -16,12 +16,12 @@ const findKey = (namesX, callback) => {
   }
 };
 
-console.log(assertEqual(findKey({
+assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2),"noma")); // => "noma"
+}, x => x.stars === 2),"noma"); // => "noma"
 

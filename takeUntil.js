fc1bf71a4f1575f01651aa262bb70107e63d5e63
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 20:46:26 +0000

    return slice of the array with elements taken from the beginning

--- takeUntil.js
+++ takeUntil.js
@@ -1,25 +1,6 @@
-const eqArrays = function(a1, a2) {
-  if (a1.length !== a2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const takeUntil = function(array, callback) {
   const idx = array.findIndex(callback);
   return array.slice(0, idx);
 };
+
+module.exports = takeUntil;

d0c64e8e47ed4d063c5111297b3fcaaba23062a9
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 14:17:59 +0000

    remove first item update Test mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,14 @@
-const tail = require ('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+describe("#tail", () => {
+
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+});

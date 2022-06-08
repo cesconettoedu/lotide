afe76cb276bc23aec0da87fabce08c38a38ec535
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 04:35:54 +0000

    take array, return the middle-most element(s)

--- middle.js
+++ middle.js
@@ -1,23 +1,22 @@
-const eqArrays = function(a1, a2) {
-  if (a1.length !== a2.length) {
-    return false;
-  }
+// const eqArrays = function(a1, a2) {
+//   if (a1.length !== a2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < a1.length; i++) {
+//     if (a1[i] !== a2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
  
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
- 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+// const assertArraysEqual = function(actual, expected) {
+//   if (eqArrays(actual, expected)) {
+//     console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+//   }
+// };
 
 const middle = function(array) {
   let output = [];
@@ -31,10 +30,6 @@ const middle = function(array) {
   }
   return output;
 };
-// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
-// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]));
-// console.log(assertArraysEqual(middle([1, 2, 3]), [2])); // => [2]
-// console.log(assertArraysEqual(middle([1]), [])); // => []
-// console.log(assertArraysEqual(middle([1, 2]), [])); // => []
-// console.log(assertArraysEqual(middle([1, 2, 3]), [2])); // => [2]
-// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [1, 2])); // => [3, 4]
+
+module.exports = middle;
+

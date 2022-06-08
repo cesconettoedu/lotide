1a23052035ae06a83227abfbcc8d53b2dbb2e19f
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 18:32:38 +0000

    collection of items and return counts for a specific items

--- countOnly.js
+++ countOnly.js
@@ -1,6 +1,3 @@
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
- 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -22,27 +19,6 @@ const countOnly = function(allItems, itemsToCount) {
         results[item] = 1;
       }
     }
-    // console.log(item);
   }
   return results;
 };
- 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
- 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
- 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
- 

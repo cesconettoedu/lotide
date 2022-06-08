bb8cee6a1dc8a4c8687aa263692784177d6479fa
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 20:45:51 +0000

    our own version of map

--- map.js
+++ map.js
@@ -1,24 +1,3 @@
-const eqArrays = function(a1, a2) {
-  if (a1.length !== a2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -26,3 +5,5 @@ const map = function(array, callback) {
   }
   return results;
 };
+
+module.exports = map;

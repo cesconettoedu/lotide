b7effca17301792f6aaf2b9360d0aed10c94adb1
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 18:47:39 +0000

    return count of each of the letters in that sentence

--- countLetters.js
+++ countLetters.js
@@ -1,9 +1,9 @@
 
 const countLetters = (array1) => {
   let output = {};
-  let arrayDiv = array1.replace(/\s/g, ''); 
+  let arrayDiv = array1.replace(/\s/g, '');
   for (let value of arrayDiv) {
-      if (value in output) {
+    if (value in output) {
       output[value] += 1;
     } else {
       output[value] = 1;

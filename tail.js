80a076ce0327a3804afe751564f375159c9ad202
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 18:41:35 +0000

    remove item, not modify array original - update clean

--- tail.js
+++ tail.js
@@ -2,9 +2,4 @@ const tail = function(rest) {
   return rest.slice(1);
 };
 
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words);
-// assertEqual(words.length, 3);
-
-
 module.exports = tail;

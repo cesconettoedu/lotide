02a837685964dc4cc43e015e977e913714aa96ca
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 20:35:44 +0000

    compare 2 obj

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,33 +1,5 @@
-const eqArrays = function(a1, a2) {
-  if (a1.length !== a2.length) {
-    return false;
-  }
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
- 
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
-  for (let value1 in object1) {
-    if (Array.isArray(object1[value1]) && Array.isArray(object2[value1]))  {
-      if (!eqArrays(object1[value1], object2[value1])) {
-        return false;
-      }
-    } else {
-      if (object1[value1] !== object2[value1]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
- 
+const eqObjects = require('./eqObjects');
+
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
@@ -37,3 +9,4 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
  
+module.exports = assertObjectsEqual;

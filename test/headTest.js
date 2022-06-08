ad05ed091cfcbb81d200b1d5b976cb2fb032d0f4
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 04:59:00 +0000

    update to teste mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,14 @@
-const head = require ('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+});

9e229d0d2c095e0ca9e3efe8ede491b2eeddc83d
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 20:44:14 +0000

    return a flattened version of the array.

--- flatten.js
+++ flatten.js
@@ -1,24 +1,3 @@
-const eqArrays = function(a1, a2) {
-  if (a1.length !== a2.length) {
-    return false;
-  }
- 
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
- 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
- 
 const flatten = function(array1) {
   if (Array.isArray(array1) === false) {
     return "its not an array";
@@ -36,3 +15,5 @@ const flatten = function(array1) {
   }
   return newArray;
 };
+
+module.exports = flatten;

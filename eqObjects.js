97f65718e65f3081ab0eae552f8234e06c8d91e5
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 18:49:20 +0000

    Implement eqObjects comparing array data.

--- eqObjects.js
+++ eqObjects.js
@@ -19,15 +19,15 @@ const eqArrays = function(a1, a2) {
   return true;
 };
 
-const eqObjects = function(object1, object2) {  
+const eqObjects = function(object1, object2) {
 
-  if (Object.keys(object1).length !== Object.keys(object2).length) {  
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
 
   for (let value1 in object1) {
-    if (Array.isArray(object1[value1]) && Array.isArray(object2[value1]))  { 
-      if (!eqArrays(object1[value1], object2[value1])) { 
+    if (Array.isArray(object1[value1]) && Array.isArray(object2[value1])) {
+      if (!eqArrays(object1[value1], object2[value1])) {
         return false;
       }
     } else {

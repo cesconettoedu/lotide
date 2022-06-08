b8b9c48ead9daca0e424a2980711ede626a59bc9
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 18:25:34 +0000

    return string where each character is found(indice).

--- letterPositions.js
+++ letterPositions.js
@@ -1,23 +1,4 @@
-// const eqArrays = function(a1, a2) {
-//   if (a1.length !== a2.length) {
-//     return false;
-//   }
 
-//   for (let i = 0; i < a1.length; i++) {
-//     if (a1[i] !== a2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = function(actual, expected) {
-//   if (eqArrays(actual, expected)) {
-//     console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-//   }
-// };
 
 const letterPositions = function(sentence) {
   const results = {};
@@ -26,12 +7,12 @@ const letterPositions = function(sentence) {
       continue;
     } else if (sentence[i] in results) {
       results[sentence[i]].push(i);
-    } else (results[sentence[i]] = [i]); {
-    }
+    } else (results[sentence[i]] = [i]);
   }
   return results;
 };
     
+letterPositions("lighthouse in the house");
 console.log(letterPositions("lighthouse in the house"));
 // console.log(assertEqual(letterPositions("lighthouse in the house")));
 

9898d9c114dbe60a90ef6523d579fb2ed496bfa2
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 18:38:47 +0000

    return string where each character is found(indice).

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,3 @@
-
-
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
@@ -12,19 +10,3 @@ const letterPositions = function(sentence) {
   return results;
 };
     
-letterPositions("lighthouse in the house");
-console.log(letterPositions("lighthouse in the house"));
-// console.log(assertEqual(letterPositions("lighthouse in the house")));
-
-// {
-//   l: [0],
-//   i: [1, 11],
-//   g: [2],
-//   h: [3, 5, 15, 18],
-//   t: [4, 14],
-//   o: [6, 19],
-//   u: [7, 20],
-//   s: [8, 21],
-//   e: [9, 16, 22],
-//   n: [12]
-// }

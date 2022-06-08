bbd0342b4d0c3103017787cf7d023c225acac524
duducesconetto <cesconettoedu@gmail.com>
2022-06-08 18:40:51 +0000

    take array, return the middle-most element(s)

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-// const eqArrays = function(a1, a2) {
-//   if (a1.length !== a2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < a1.length; i++) {
-//     if (a1[i] !== a2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
- 
-// const assertArraysEqual = function(actual, expected) {
-//   if (eqArrays(actual, expected)) {
-//     console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-//   }
-// };
-
 const middle = function(array) {
   let output = [];
   if (array.length < 3) {
